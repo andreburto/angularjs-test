10735ed44fde2b13daff503ede5df30c8f5edd63
Andrew Burton <tuglyraisin@aol.com>
2016-01-05 22:18:37 -0600

    small changes to 3

--- checkbox1.php
+++ checkbox1.php
@@ -2,7 +2,7 @@
 
 $json_input = json_decode(file_get_contents('php://input'), true);
 
-$var = array('checked'  =>  (isset($json_input['checked'])?$json_input['checked']:'no'),
+$var = array('checked'  =>  (isset($json_input['checked'])?$json_input['checked']:'No'),
              'thetime'  =>  date("Y-m-d H:i:s"));
 
 $json_output = json_encode($var);
